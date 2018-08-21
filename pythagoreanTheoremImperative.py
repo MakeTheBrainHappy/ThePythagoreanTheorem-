db67b72b9d26b91d432d8aa601c9237d0a7d14d9
MakeTheBrainHappy <makethebrainhappy@gmail.com>
2018-08-21 17:38:48 -0400

    Added an Imperative Version

---
+++ pythagoreanTheoremImperative.py
@@ -0,0 +1,8 @@
+import math #imports the square root function
+
+def main():
+	a = 1 #first side
+	b = 2 #second side
+	c = math.sqrt(a**2 + b**2) #calculates the third side of the triangle
+	print(c) #final answer
+main()
